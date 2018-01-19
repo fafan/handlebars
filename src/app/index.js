c5fe01592c25c28ec13c718cdb693cdc5635755d
Fafan <fafan@mac.local>
2018-01-19 15:38:47 +0700

    Update index template

--- src/app/index.js
+++ src/app/index.js
@@ -2,15 +2,12 @@ require('../lib/jquery.min.js')
 require('../lib/underscore.min.js')
 require('../lib/backbone.min.js')
 
-
-var indexTemplate = require("../template/index.handlebars")
-indexTemplate({
-  title: 'Handlebars Application'
-})
-
-var Router = require('./router.js')
+require("./index.html")
+require("./index.css")
 
 $(document).ready(function($){
-  new Router();
-  Backbone.history.start();
-});
+  document.title = 'Handlebars Application'
+  var Router = require('./router.js')
+  new Router()
+  Backbone.history.start()
+})
